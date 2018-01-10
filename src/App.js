d4d54dace57e26574c2fa64d0402414a3f10e02a
krk chaitanya <krkchaitanya42@gmail.com>
2018-01-10 16:45:50 -0600

    RoutingInit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import {Route, Switch} from "react-router-dom";
+
 import './App.css';
 import Layout from "./components/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
@@ -9,8 +11,10 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <BurgerBuilder/>
-          <Checkout/>
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" exact component={BurgerBuilder}/>
+          </Switch>
         </Layout>  
       </div>
     );
--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,20 +1,23 @@
-import React from "react";
+import React from 'react';
 
-import Burger from "../../Burger/Burger";
-import Button from "../../UI/Button/Button";
-import classes from "./CheckoutSummary.css";
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+import classes from './CheckoutSummary.css';
 
-const checkoutSummary=(props)=>{
-
-    return(
+const checkoutSummary = (props) => {
+    return (
         <div className={classes.CheckoutSummary}>
-            <h1>Hope it tastes well</h1>
-            <div style={{width:"300px",height:"300px",margin:"auto"}}>
-                <Burger  ingredients={props.ingredients}/>
-            </div>    
-            <Button btnType="Danger" clicked>Cancel</Button>
-            <Button btnType="Success" clicked>Continue</Button>
-        </div>    
+            <h1>We hope it tastes well!</h1>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Burger ingredients={props.ingredients}/>
+            </div>
+            <Button 
+                btnType="Danger"
+                clicked>CANCEL</Button>
+            <Button 
+                btnType="Success"
+                clicked>CONTINUE</Button>
+        </div>
     );
 }
 
