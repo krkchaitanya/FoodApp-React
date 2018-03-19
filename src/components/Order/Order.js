1042758c047ab3de319e47753df415b36d321138
krk chaitanya <krkchaitanya42@gmail.com>
2018-03-18 23:50:21 -0500

    orderComponentUpdate

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -28,7 +28,7 @@ const order=(props)=>{
          <div className={classes.Order}>
 
              <p>Ingredients: {ingredientOutput}</p>
-             <p>Price:<strong>USD {props.price}</strong></p>
+             <p>Price:<strong>USD {Number.parseFloat( props.price ).toFixed( 2 )}}</strong></p>
          </div>    
      )
 }
